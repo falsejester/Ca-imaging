5db15bb0d36ee9e214aae0ed06feeeb65e838d82
falsejester <54432570+falsejester@users.noreply.github.com>
2019-10-11 16:14:28 +0900

    Add files via upload

---
+++ xlsdatain.m
@@ -0,0 +1,8 @@
+function Data = xlsdatain(varargin)
+    n = max(size(varargin));
+    for i=1:n
+        eval(['Data', int2str(i),' = xlsread(varargin{i})']);
+        eval(['save Data',int2str(i),'.mat']) 
+    end
+    save
+end
